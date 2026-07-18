b55d15a8e7be60217939a8634dbc7a3727a151c2
sod3 <mudassir.muneer123@gmail.com>
2026-07-18 20:37:55 +0500

    Fix Greek service section layout

--- src/components/wp-clone/WpClonePage.tsx
+++ src/components/wp-clone/WpClonePage.tsx
@@ -22,7 +22,25 @@ type RuntimeWpClonePage = Omit<WpClonePageData, "locale"> & {
   };
 };
 
+const greekAiSeoStats = `
+<div class="perf-stats">
+  <div class="perf-stat"><h4>98<span>%</span></h4><p>ΠΟΣΟΣΤΟ ΕΠΙΤΥΧΙΑΣ</p></div>
+  <div class="perf-stat"><h4>2.5<span>x</span></h4><p>ΜΕΣΗ ΑΠΟΔΟΣΗ</p></div>
+  <div class="perf-stat"><h4>10<span>k+</span></h4><p>ΛΕΞΕΙΣ-ΚΛΕΙΔΙΑ</p></div>
+</div>`;
+
+function normalizeServiceSectionHtml(page: RuntimeWpClonePage) {
+  if (page.path !== "/el/ai-seo-4/" || page.html.includes('class="perf-stats"')) {
+    return page.html;
+  }
+
+  const chartEnd = '<div class="perf-bar" style="height: 90%;"></div></div></div>';
+  return page.html.replace(chartEnd, `${chartEnd}${greekAiSeoStats}`);
+}
+
 export function WpClonePage({ page }: { page: RuntimeWpClonePage }) {
+  const html = normalizeServiceSectionHtml(page);
+
   return (
     <div
       className={`wp-clone-root ${page.bodyClass}`}
@@ -32,7 +50,7 @@ export function WpClonePage({ page }: { page: RuntimeWpClonePage }) {
       data-page-path={page.path}
       suppressHydrationWarning
     >
-      <div dangerouslySetInnerHTML={{ __html: page.html }} />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
       <BackToTopButton locale={page.locale} />
       <WpCloneBehavior locale={page.locale} pagePath={page.path} />
     </div>
